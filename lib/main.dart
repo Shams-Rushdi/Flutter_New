fd43de96a3fb3cf0320e0bead83e21dd1656f21d
Abu Rushed Md Bin Shams <77982841+Shams-Rushdi@users.noreply.github.com>
2022-09-06 11:11:55 +0600

    raw gmail style

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main(){
   runApp(MyApp());
@@ -36,7 +37,9 @@ class MyApp extends StatelessWidget{
 
 
 class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
+  //const MyHome({Key? key}) : super(key: key);
+
+  List<String> userList=["All inboxes" ,"Primary","Promotions", "Social", "Starred", "Snoozed", "Important", "Sent", "Scheduled", "Outbox", "Drafts", "All Mail" , "Spam", "Bin", "Unwanted", "Calender", "Contacts", "Settings", "Help and feedback"] ;
 
   @override
   Widget build(BuildContext context) {
@@ -45,19 +48,74 @@ class MyHome extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page') ,
         backgroundColor: Colors.amberAccent,
+        elevation: 10.0,
+        actions: [
+          IconButton(onPressed: (){Fluttertoast.showToast(msg: "Shopping Bag");}, icon: Icon(Icons.shopping_bag_outlined)),
+          IconButton(onPressed: (){Fluttertoast.showToast(msg: "Person Add");}, icon: Icon(Icons.person_add)),
+          IconButton(onPressed: (){Fluttertoast.showToast(msg: "Searching");}, icon: Icon(Icons.search_outlined)),
+        ],
+
       ),
       body: Container(
         color: Colors.white24,
       ),
-      drawer:const Drawer() ,
-      //backgroundColor: Colors.black,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+                accountName: const Text("Abu Rushed Md Bin Shams"),
+                accountEmail: const Text("aburushed@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+                
+            ),
+
+
+            ListTile(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              title: Text(userList[0]),
+              leading: CircleAvatar(
+                child: Icon(Icons.home)
+              ) ,
+
+              trailing: Text("101"),
+              subtitle: Text("Go Home Page"),
+            ),
+            const Divider(height: 1,),
+            ListTile(
+              onTap: (){Navigator.pop(context);},
+              title: Text(userList[1]),
+              leading: CircleAvatar(
+                child: Icon(Icons.category)
+              ) ,
+
+              trailing: Text("56"),
+              subtitle: Text("Go Category List"),
+            ),
+            const Divider(height: 1,),
+            ListTile(
+              onTap: (){Navigator.pop(context);},
+              title: Text(userList[2]),
+              leading: CircleAvatar(
+                  child: Icon(Icons.favorite)
+              ) ,
+
+              trailing: Text("10"),
+              subtitle: Text("Go Favorite List"),
+            ),
+          ],
+        ),
+      ) ,
+      backgroundColor: Colors.black,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.amber,
         onPressed: (){
         },
-      //child: const Text("Add"), 
+      //child: const Text("Add"),
         child: const Icon(Icons.add),
-        
       ),
 
       bottomNavigationBar: BottomNavigationBar(
